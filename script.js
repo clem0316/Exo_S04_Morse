0b8b65cb055a18df01d16cb3f97507f443c6ecbe
Clément Badin <clement@macbook-air-de-clement.home>
2023-05-20 23:54:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+const morseToLatin = {
+  "-": "T",
+  "--": "M",
+  "---": "O",
+  "--.": "G",
+  "--.-": "Q",
+  "--..": "Z",
+  "-.": "N",
+  "-.-": "K",
+  "-.--": "Y",
+  "-.-.": "C",
+  "-..": "D",
+  "-..-": "X",
+  "-...": "B",
+  ".": "E",
+  ".-": "A",
+  ".--": "W",
+  ".---": "J",
+  ".--.": "P",
+  ".-.": "R",
+  ".-..": "L",
+  "..": "I",
+  "..-": "U",
+  "..-.": "F",
+  "...": "S",
+  "...-": "V",
+  "....": "H",
+};
+
+const getLatinCharacterList = (texte) => {
+  let separateText = texte.split("");
+  return separateText;
+};
+console.log(getLatinCharacterList("Hello World"));
+
+const translateLatinCharacter = (char) => {
+  let latinArray = latinToMorse[char];
+  return latinArray;
+};
+console.log(translateLatinCharacter("A"));
