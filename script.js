3cccbb9c31b8c8f9a6004b6108436f3f4141890c
Clément Badin <clement@macbook-air-de-clement.home>
2023-05-21 00:30:04 +0200

    4 étapes sur 5

--- script.js
+++ script.js
@@ -56,14 +56,49 @@ const morseToLatin = {
   "....": "H",
 };
 
-const getLatinCharacterList = (texte) => {
-  let separateText = texte.split("");
+// Séparer les caractères d'un texte
+const getLatinCharacterList = (text) => {
+  let separateText = text.split("");
   return separateText;
 };
-console.log(getLatinCharacterList("Hello World"));
 
+// Correspondre une lettre avec interprétation Morse
 const translateLatinCharacter = (char) => {
   let latinArray = latinToMorse[char];
   return latinArray;
 };
-console.log(translateLatinCharacter("A"));
+
+// Encoder en Morse un message texte
+const encode = (text) => {
+  let upText = text.toUpperCase();
+
+  let result = [];
+  let sentence = getLatinCharacterList(upText);
+  for (let i = 0; i < sentence.length; i++) {
+    result.push(translateLatinCharacter(sentence[i]));
+  }
+  return result;
+};
+
+// Séparer les caractères Morse (que l'on a séparés par des espaces dans notre paramètre d'entrée)
+const getMorseCharacterList = (morse) => {
+  let separateMorse = morse.split(" ");
+  return separateMorse;
+};
+
+// Correspondre le caractère Morse avec son équivalent en texte
+const translateMorseCharacter = (sign) => {
+  let morseArray = morseToLatin[sign];
+  return morseArray;
+};
+
+// Décoder le message Morse en texte
+const decode = (sign) => {
+  let result = [];
+  let sentence = getMorseCharacterList(sign);
+  for (let i = 0; i < sentence.length; i++) {
+    result.push(translateMorseCharacter(sentence[i]));
+  }
+  return result;
+};
+console.log(decode("-.-. --- ..- -.-. --- ..-"));
